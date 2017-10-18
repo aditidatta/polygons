d53b91334b73fdc12310a48bc7a4ffcfa08ca2b9
Aditi Datta <aditidatta@live.com>
2017-10-18 14:21:55 -0400

    adding ui-routers

--- public/javascripts/main.controller.js
+++ public/javascripts/main.controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('polygons', []);
+var app = angular.module('polygons', ['ui.router']);
 
 app.controller('MainCtrl', [
     '$scope',
@@ -6,3 +6,34 @@ app.controller('MainCtrl', [
         $scope.test = 'Welcome to Polygons.me!';
     }
 ]);
+
+myApp.config(function($stateProvider) {
+    var home = {
+        name: 'home',
+        url: '/home',
+        template: '<h3>Welcome to Polygons.me</h3>'
+    }
+
+    var algo = {
+        name: 'algo',
+        url: '/algo',
+        template: '<h3>List of Algorithms</h3>'
+    }
+
+    var research = {
+        name: 'research',
+        url: '/research',
+        template: '<h3>My dissertation paper</h3>'
+    }
+
+    var projects = {
+        name: 'projects',
+        url: '/projects',
+        template: '<h3>List of Projects</h3>'
+    }
+
+    $stateProvider.state(home);
+    $stateProvider.state(algo);
+    $stateProvider.state(research);
+    $stateProvider.state(projects);
+});
